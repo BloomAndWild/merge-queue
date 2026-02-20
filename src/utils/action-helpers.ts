6ad6cd42f3ea656ed2f5e5798867a723212cb6e8
Josh Butterworth <josh.butterworth@bloomandwild.com>
2026-02-20 09:09:04 +0000

    Added the names of the merge-queue jobs to always be included in the ignore-checks list, removed them from the sample yml files

--- src/utils/action-helpers.ts
+++ src/utils/action-helpers.ts
@@ -76,8 +76,8 @@ export function getConfig(): QueueConfig {
     maxUpdateRetries,
     mergeMethod: mergeMethod as MergeMethod,
     deleteBranchAfterMerge: core.getInput('delete-branch-after-merge') === 'true',
-    ignoreChecks: core
-      .getInput('ignore-checks')
+    ignoreChecks: 'Add PR to Merge Queue,Remove PR from Merge Queue,Process Merge Queue,'
+      .concat(core.getInput('ignore-checks'))
       .split(',')
       .map(c => c.trim())
       .filter(Boolean),
